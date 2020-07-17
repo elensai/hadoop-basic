7c0e0417b0d26a0074a1a335c0949a0cd8b2eac2
xiaobao <luxinyue@elensdata.com>
2020-07-17 22:28:06 +0800

    update sh

--- check.sh
+++ check.sh
@@ -14,6 +14,7 @@ EOF
     echo 'exsit'
   fi
   ;;
+  
   nnfc)
   if [ ! -d /user/data/hadoop/hdfs/name ];then
     hadoop namenode -format
@@ -21,18 +22,22 @@ EOF
     scp -r /user/data/hadoop/hdfs $2:/user/data/hadoop/
   fi
   ;;
+
   flink)
   hadoop fs -test -e hdfs://ns/flink/history ; 
   if [ $? -ne 0 ]; then hadoop fs -mkdir -p hdfs://ns/flink/history ; fi 
   ;;
+
   oozie)
   hadoop fs -test -e /user/root/share/lib/* ;
   if [ $? -ne 0 ]; then  (oozie-setup.sh prepare-war sharelib create -fs hdfs://ns -locallib $OOZIE_HOME/oozie-sharelib-4.0.0-cdh5.3.6-yarn.tar.gz) && (ooziedb.sh create -sqlfile oozie.sql -run DB Connection && oozied.sh start); fi
   ;;
+
   spark)
   hadoop fs -test -e hdfs://ns/log/spark/eventlogs ;
   if [ $? -ne 0 ]; then hadoop fs -mkdir -p hdfs://ns/log/spark/eventlogs && hadoop fs -mkdir -p hdfs://ns/spark_jars/ && hadoop fs -put $SPARK_HOME/jars/*  hdfs://ns/spark_jars/; fi
   ;;
+
   *)
     echo 'other'
   ;;
